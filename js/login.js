548652b24e1a90426f79c00a5ce6d5ffd6c61b0a
Code-alpaca <58038635+Code-alpaca@users.noreply.github.com>
2019-12-02 21:44:04 +0800

    modify

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;
     var pwd=document.getElementById("pwd").value;
 		//修改密码请改此处
-    if(userName=="caiwang" &&  pwd=="1110"){
+    if(userName=="alpaca" &&  pwd=="1110"){
 			event.preventDefault();
 			$('form').fadeOut(500);
 			$('.wrapper').addClass('form-success');
@@ -12,7 +12,7 @@ $('#login-button').click(function (event) {
 			},2000);
 		}
 	else{
-		alert("emmmmmmm亲填错了哦");
+		alert("emmmmmmm女王填错了哦");
 	}
 });
 
